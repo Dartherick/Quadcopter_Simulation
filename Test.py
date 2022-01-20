875a37f04825c1bc8ebebc96aadfa93fbaae11b4
Dartherick <ericktejadacompres@gmail.com>
2022-01-19 22:25:29 -0400

    Listo

--- Test.py
+++ Test.py
@@ -3,7 +3,7 @@ from time import sleep
 import numpy as np
 import cv2
 
-Scale = 10
+Scale = 5
 
 '''
 Para enlazar coppelia sim con el codigo realizado en python,coloca la siguiente funcion en un script en coppelia
@@ -22,20 +22,38 @@ else:
 	exit()
 
 VisionSensor = sim.simxGetObjectHandle(clientID, 'Vision', sim.simx_opmode_blocking)[-1]
+Fire = sim.simxGetObjectHandle(clientID, 'fire', sim.simx_opmode_blocking)[-1]
 
 sim.simxGetVisionSensorImage(clientID,VisionSensor,0,sim.simx_opmode_streaming)
 sleep(1)
 _,Resolution,_ = sim.simxGetVisionSensorImage(clientID,VisionSensor,0,sim.simx_opmode_buffer)
+print(Resolution)
+
+Low = np.array([0,0,0])
+High = np.array([0,0,0])
 
 while True:
-	_, _, RGB = sim.simxGetVisionSensorImage(clientID,VisionSensor,0,sim.simx_opmode_buffer) 
-	Img = np.array(RGB,dtype=np.uint8)					#Convert the image list into a numpy array
-	Img.resize([Resolution[0],Resolution[1],3])			#Convert the numpy array into a image
-	Img = cv2.cvtColor(Img,cv2.COLOR_BGR2RGB)			#Change BGR to RGB	
-	Img = cv2.resize(Img,np.array(Resolution)*Scale)	#Scale the image
-	cv2.imshow('Thermal camera',Img)
+	_, _, Frames = sim.simxGetVisionSensorImage(clientID,VisionSensor,0,sim.simx_opmode_buffer) 
 	
+	FRAMEBGR = np.array(Frames,dtype=np.uint8)					#Convert the image list into a numpy array
+	FRAMEBGR.resize([Resolution[0],Resolution[1],3])			#Convert the numpy array into a image
+	FRAMEBGR = cv2.resize(FRAMEBGR,np.array(Resolution)*Scale)	#Scale the image
+
+	FRAMERGB = cv2.cvtColor(FRAMEBGR,cv2.COLOR_BGR2RGB)			#Change BGR to RGB	
+	FrameGray = cv2.cvtColor(FRAMERGB,cv2.COLOR_RGB2GRAY)		#Change BGR to RGB	
+	FrameHSV = cv2.cvtColor(FRAMEBGR,cv2.COLOR_RGB2HSV)
+	
+	FireMask = cv2.inRange(FrameHSV,Low,High)
+	#FireMask = cv2.bitwise_and(FRAMEBGR, FRAMEBGR, mask= FireMask)  
+
+	cv2.imshow('Fire Mask', FireMask)
+	cv2.imshow('Camara',FRAMERGB)
+	cv2.imshow('Gray',FrameGray)
+
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
 
 cv2.destroyAllWindows()
+
+sleep(5)
+#sim.simxSetObjectPosition(clientID,Fire,-1,(0,0,0),sim.simx_opmode_oneshot)
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import re
 from Dron import Quadcopter_Drone
 import sim
 import math
 from time import sleep
+import numpy as np
 
 '''
 Para enlazar coppelia sim con el codigo realizado en python,coloca la siguiente funcion en un script en coppelia
@@ -19,31 +21,160 @@ else:
 	print("Not connected to remote API server")
 	exit()
 
-AreaSize = (5,5) #Area size of the simulation in meters
-Quadcopter = Quadcopter_Drone(clientID,(1,1))
+def StartSimulation():
+	sim.simxStartSimulation(clientID,sim.simx_opmode_oneshot) 	
 
-for i in range(AreaSize[0]):
-	sim.simxGetObjectPosition()
+def StopSimulation():
+	sim.simxPauseSimulation(clientID,sim.simx_opmode_oneshot) 	
 
 
-def Hipotenusa():
-	Dummy = sim.simxGetObjectHandle(clientID, 'Quadcopter_target', sim.simx_opmode_blocking)[-1]
-	sim.simxGetObjectPosition(clientID, Dummy, -1, sim.simx_opmode_streaming)
-	sleep(0.2)
-	Position = sim.simxGetObjectPosition(clientID, Dummy, -1, sim.simx_opmode_buffer)[-1]
-	print(Position)
+Drone = Quadcopter_Drone(clientID,(1,1))
+AreaSize = (5,5)
+Start = sim.simxGetObjectHandle(clientID, 'Dummy', sim.simx_opmode_blocking)[-1]
+Target = sim.simxGetObjectHandle(clientID, 'Quadcopter_target', sim.simx_opmode_blocking)[-1]
+Body = sim.simxGetObjectHandle(clientID, 'Quadcopter', sim.simx_opmode_blocking)[-1]
+Fire = sim.simxGetObjectHandle(clientID, 'Fire', sim.simx_opmode_blocking)[-1]
 
-	FinalPosition = (0,0,Position[-1])
+sim.simxGetObjectPosition(clientID, Target, Start, sim.simx_opmode_streaming)
+TargetPosition = sim.simxGetObjectPosition(clientID, Target, Start, sim.simx_opmode_buffer)[-1]
 
-	CatetoX = abs(Position[0] - FinalPosition[0]) 
-	CatetoY = abs(Position[1] - FinalPosition[1]) 
-	Hipotenusa = math.sqrt(CatetoX**2 + CatetoY**2)
+sim.simxGetObjectOrientation(clientID,Target,Start,sim.simx_opmode_streaming)
 
-	b = math.radians(math.asin(CatetoX/Hipotenusa))
+sim.simxGetObjectPosition(clientID, Body, Start, sim.simx_opmode_streaming)
+sim.simxGetObjectPosition(clientID, Fire, Start, sim.simx_opmode_streaming)
+sleep(0.2)
 
-	for a in range(91):
-		X = ((math.atan(b) * Position[0] + math.tan(a) * FinalPosition[0]) / (math.atan(b)+math.tan(a)))
-		Y = ((math.atan(b) * Position[0] + math.tan(a) * FinalPosition[0]) / (math.atan(b)+math.tan(a)))
-		print(f'X={X}  Y={Y}')
-		#sleep(1)
-		#sim.simxSetObjectPosition(clientID, Dummy, -1, (X,Y,Position[-1]),sim.simx_opmode_oneshot)
+
+def ValidatePosition():
+	while True:
+		BodyPosition = sim.simxGetObjectPosition(clientID, Body, Start, sim.simx_opmode_buffer)[-1]
+		BodyPosition = [round(x,2) for x in BodyPosition]
+		
+		TargetPosition = sim.simxGetObjectPosition(clientID, Target, Start, sim.simx_opmode_buffer)[-1]
+		TargetPosition = [round(x,2) for x in TargetPosition]
+
+		if BodyPosition == TargetPosition:
+			break
+
+def ValidateOrientation():
+	while True:
+		BodyOrientation = sim.simxGetObjectOrientation(clientID,Body,Start,sim.simx_opmode_buffer)[-1]
+		BodyOrientation = [round(x,2) for x in BodyOrientation]
+		
+		TargetOrientation = sim.simxGetObjectOrientation(clientID,Target,Start,sim.simx_opmode_buffer)[-1]
+		TargetOrientation = [round(x,2) for x in TargetOrientation]
+
+		if BodyOrientation == TargetOrientation:
+			break
+
+def DetectFire():
+	TargetPosition = sim.simxGetObjectPosition(clientID, Body, Start, sim.simx_opmode_buffer)[-1]
+	TargetPosition = [round(x,0) for x in TargetPosition]
+
+	FirePosition = sim.simxGetObjectPosition(clientID, Fire, Start, sim.simx_opmode_buffer)[-1]
+	FirePosition = [round(x,0) for x in FirePosition]
+	
+	#print(f'Target {TargetPosition[0:2]} ; Fire {FirePosition[0:2]}')
+	print(f'Drone position {TargetPosition}')
+
+	if FirePosition[0] == TargetPosition[0]:
+		if FirePosition[1] == (TargetPosition[1]+1):
+			return True
+	
+Altura = 2.4
+
+Z = list(np.arange(0, Altura, 0.2))
+
+for Depth in Z:
+	sleep(0.05)
+	sim.simxSetObjectPosition(clientID,Target,Start,[0,0,Depth],sim.simx_opmode_oneshot)
+
+
+X = list(np.arange(0, 0.1, 0.005))
+Y = list(np.arange(0, 5, 0.05))
+referencia = sim.simxGetObjectOrientation(clientID,Target,Start,sim.simx_opmode_buffer)[-1]
+Ubicacion = sim.simxGetObjectOrientation(clientID,Target,Start,sim.simx_opmode_buffer)[-1]
+RotationAngle = list(np.arange(referencia[-1], 0, 0.0001))
+RotationAngle1 = list(np.arange(-1.570796012878418, 0, 0.0001))
+
+sleep(2)
+
+i,j = 0,0
+SleepTime = 0.05
+SleepTime1 = 0.03
+
+Detected = False
+
+while True:
+	#Izq a Derecha
+	for j in Y:
+		sleep(SleepTime)
+		sim.simxSetObjectPosition(clientID,Target,Start,[i,j,Altura],sim.simx_opmode_oneshot)
+		Detected = DetectFire()
+		if Detected: 
+			break
+		#ValidatePosition()
+
+	if Detected: 
+		break
+
+	sleep(1)
+	
+	#Rotacion 45 grados a izq
+	for Angle in RotationAngle:
+		sim.simxSetObjectOrientation(clientID,Target,Start,[0,0,Angle],sim.simx_opmode_oneshot)
+	
+	sleep(1)
+
+	#Mov eje x
+	for Temp in X:
+		i -= Temp
+		sleep(SleepTime1)
+		sim.simxSetObjectPosition(clientID,Target,Start,[i,j,Altura],sim.simx_opmode_oneshot)
+		#ValidatePosition()
+	
+	sleep(1)
+	
+	#Rotacion 45 grados a izq
+	for Angle in reversed(RotationAngle1):
+		sim.simxSetObjectOrientation(clientID,Target,Start,[0,0,-Angle],sim.simx_opmode_oneshot)
+	
+	sleep(1)
+	
+	#Mov eje -Y
+	for j in reversed(Y):
+		sleep(SleepTime)
+		sim.simxSetObjectPosition(clientID,Target,Start,[i,j,Altura],sim.simx_opmode_oneshot)
+		Detected = DetectFire()
+		if Detected: 
+			break
+		#ValidatePosition()
+
+	if Detected: 
+		break
+
+	sleep(1)
+	
+	for Angle in RotationAngle1:
+		sim.simxSetObjectOrientation(clientID,Target,Start,[0,0,-Angle],sim.simx_opmode_oneshot)
+
+	for Temp in X:
+		i -= Temp
+		sleep(SleepTime1)
+		sim.simxSetObjectPosition(clientID,Target,Start,[i,j,Altura],sim.simx_opmode_oneshot)
+		#ValidatePosition()
+
+	sleep(1)
+
+	#Rotacion 45 grados a izq
+	for Angle in reversed(RotationAngle):
+		sim.simxSetObjectOrientation(clientID,Target,Start,[0,0,Angle],sim.simx_opmode_oneshot)
+
+FirePosition = sim.simxGetObjectPosition(clientID, Fire, Start, sim.simx_opmode_buffer)[-1]
+FirePosition = [round(x,3) for x in FirePosition[0:2]]
+
+DialogHandle = sim.simxDisplayDialog(clientID,'Fuego detectado!','Fuego detectado en la ubicacion '+str(FirePosition),sim.sim_dlgstyle_message,'',None,None,sim.simx_opmode_blocking)[1]
+sleep(5)
+sim.simxEndDialog(clientID,DialogHandle,sim.simx_opmode_oneshot)
+sim.simxStopSimulation(clientID,sim.simx_opmode_oneshot)
+print('\nFin de la simulacion')
